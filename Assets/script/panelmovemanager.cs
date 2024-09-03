dc8f0b66781cda933548cb261f26b9ab66b935b1
408-07\student <FCA22090022@edu.fca.ac.jp>
2024-09-03 19:37:30 +0900

    no message

--- Assets/script/panelmovemanager.cs
+++ Assets/script/panelmovemanager.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
+    [SerializeField] GameObject panel;
     // Update is called once per frame
     void Update()
     {
-        if ()
+
+        if (Input.GetKey(KeyCode.W))
         {
-            ;
+          panel.transform.position=panel.transform.position*15;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            panel.transform.position = panel.transform.position * 15;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            panel.transform.position = panel.transform.position * 15;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            panel.transform.position = panel.transform.position * 15;
         }
     }
 }
